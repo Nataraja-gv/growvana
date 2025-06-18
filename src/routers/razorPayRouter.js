e93eb0c76d4c6845f5015a132176a9a3931d4a64
Nataraja-gv <nataraja2k@gmail.com>
2025-06-18 15:48:52 +0530

    commit

--- src/routers/razorPayRouter.js
+++ src/routers/razorPayRouter.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const userAuth = require("../middleware/userauth");
 const { RazorPayOrderController, RazorPayVerify } = require("../controllers/razorpayControllers");
- 
- 
-
-const razorPayrouter = express.Router();
+ const razorPayrouter = express.Router();
 
 razorPayrouter.post(
   "/user/order/razorpay/create",
