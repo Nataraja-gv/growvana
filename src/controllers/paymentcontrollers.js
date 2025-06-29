29aebdae807a778e90027df9cc265fc4d9991d37
Nataraja-gv <nataraja2k@gmail.com>
2025-06-29 12:45:41 +0530

    ---

--- src/controllers/paymentcontrollers.js
+++ src/controllers/paymentcontrollers.js
@@ -230,9 +230,12 @@ const RazorPayPremiumVerify = async (req, res) => {
 
     const paymentDetails = req.body.payload.payment.entity;
 
+
     const order = await SubScription.findOne({
       "razorpayDetails.orderId": paymentDetails?.order_id,
     });
+    console.log(paymentDetails,"paymentDetails");
+    console.log(order,"order")
 
     if (!order) {
       return res.status(404).json({ message: "Order not found" });
@@ -242,9 +245,9 @@ const RazorPayPremiumVerify = async (req, res) => {
     order.razorpayDetails.signature = webhookSignature;
 
     await order.save();
-    const user = await User.findById({ _id: userId });
-    user.isPremium = newSubScription.planType ? true : false;
-    await user.save();
+    // const user = await User.findById({ _id: userId });
+    // user.isPremium = newSubScription.planType ? true : false;
+    // await user.save();
 
     res.status(200).json({ message: "webhook received successfully" });
   } catch (error) {
